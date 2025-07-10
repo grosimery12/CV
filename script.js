4974b27eaf7554b44ba3577ec31dad265642893b
Rosimery <grosimery12@gmail.com>
2025-07-10 18:39:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("download-btn").addEventListener("click", () => {
   const element = document.getElementById("cv");
   const opt = {
     margin: 0,
-    filename: "John_William_CV.pdf",
+    filename: "Rosimery_Gomes_CV.pdf",
     image: {
       type: "jpeg",
       quality: 0.98,
